b30e5c534a5edd9af61f590b700b5af2c21759ee
KEdge <kmedge22@users.noreply.github.com>
2017-03-16 21:09:28 -0400

    Update Board.java
    The first version of the game was built primarily for young children who could not read. This version accommodates readers as well. Consequently, in addition to the colored cards it already has, the game includes a second kind of card. These cards are not colored. Instead, the cards feature the name of the color. i.e. "red", "yellow", "green", "blue", "magenta", or "orange". A text card only matches another text card with the same color name, and a color card only matches another color card that features the same color. All other comparisons result in a mismatch.
    When the game is started, the user specifies (in some convenient way) how many pairs of cards they want to use for that game. For example, the user may want 10 pairs of cards, in which case your game features 20 cards. The cards should be laid out in an arrangement as close to square as possible. For example, 20 cards would be laid out in a 5x4 pattern.
    Each game includes a random mix of both kinds of cards.

--- Board.java
+++ Board.java
@@ -7,23 +7,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class Board {
     private final List<Card> cardList;
     private final Rules createGame;
     private final JFrame frame;
+    private int rows;
+    private int cols;
+    private int pairsNumFromUser;
 
     Board() {
         //menuBar
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("Menu");
         JMenuItem reset = new JMenuItem("Reset");
-        
+
         //Dialog Box to determine number of pairs to play with
         JFrame dFrame = new JFrame();
         Object numberOfPairs = JOptionPane.showInputDialog(dFrame, "Enter number of pairs you want to play with: ");
 
-        
         //Sets reaction to clicking reset from the menuBar
         reset.addActionListener(new ActionListener() {
             @Override
@@ -39,56 +42,110 @@ public class Board {
 
         //ranks: what will be compared to determine match
         Color[] colorArray = {Color.GREEN, Color.BLUE, Color.MAGENTA, Color.ORANGE, Color.RED, Color.YELLOW};
+        String[] stringArray = {"red", "yellow", "green", "blue", "magenta", "orange"};
         cardList = new ArrayList<>();
         List<Color> colors = Arrays.asList(colorArray);
 
+        //determine how many rows and columns
+        //take object and turn it to an integer
+        determineSquare(Integer.valueOf(Integer.parseInt(numberOfPairs.toString())));
+        this.pairsNumFromUser = Integer.valueOf(Integer.parseInt(numberOfPairs.toString()));
+
         //setUp frame and panel
         frame = new JFrame();//creating instance of JFrame
-        frame.setSize(400, 500);//400 width and 500 height
+        frame.setSize(500, 500);//400 width and 500 height
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        createGame = new Rules(frame);
+        createGame = new Rules(frame, pairsNumFromUser);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3,4,5,5));
-
-        //for every rank (color) in array, assign one rank (color) to two separate cards
-        for (Color color : colors) {
-            cardList.add(new Card(color));
-            cardList.add(new Card(color));
-        }
-
-        //for each card in cardList, set an ActionListener
-        cardList.forEach(e->e.addActionListener(new Listener()));
+        panel.setLayout(new GridLayout(rows, cols, 5, 5));
 
         //builtin shuffle
         Collections.shuffle(cardList);
 
-        //for every card in cardList, add it to panel
-        cardList.forEach(panel::add);
+        //for every rank (color) in array, assign one rank (color) to two separate cards
+        for (Color color : colors) {
+            cardList.add(new Card(color));
+        }
+            for (String colorString : stringArray) {
+                cardList.add(new Card(colorString));
+            }
 
-        //place panel and menuBar in frame
-        frame.add(panel, BorderLayout.CENTER);
-        frame.add(menuBar, BorderLayout.NORTH);
-        frame.pack();
-        frame.setVisible(true);//making the frame visible
-    }
+            //iterate through the list and ensure index doesn't run out of range
+            List<Card> game = new ArrayList<>();
+            for (int i = 0; i < pairsNumFromUser; i++) {
+                if (cardList.get(i%cardList.size()).getMyColor()==Color.white ) {
+                    game.add(new Card(cardList.get(i%cardList.size()).getStringColor()));
+                    game.add(new Card(cardList.get(i%cardList.size()).getStringColor()));
+                }
+                else {
+                    game.add(new Card(cardList.get(i%cardList.size()).getMyColor()));
+                    game.add(new Card(cardList.get(i%cardList.size()).getMyColor()));
+                }
+            }
 
-    /**
-     * find object of click event. If a isn't empty and is matched
-     * then talk to Rules for instruction
-     */
-    public class Listener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Card cardCliked = (Card)e.getSource();
-            if (cardCliked != null && !cardCliked.isMatched()) {
-                createGame.cardController(cardCliked);
+            //shuffle
+            Collections.shuffle(game);
+
+            //for each card in cardList, set an ActionListener
+            game.forEach(e -> e.addActionListener(new Listener()));
+
+            //for every card in game List, add it to panel
+            game.forEach(panel::add);
+
+            //place panel and menuBar in frame
+            frame.add(panel, BorderLayout.CENTER);
+            frame.add(menuBar, BorderLayout.NORTH);
+            frame.pack();
+            frame.setVisible(true);//making the frame visible
+        }
+        /**
+         * find object of click event. If a isn't empty and is matched
+         * then talk to Rules for instruction
+         */
+        public class Listener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Card cardCliked = (Card) e.getSource();
+                if (cardCliked != null && !cardCliked.isMatched()) {
+                    createGame.cardController(cardCliked);
+                }
             }
         }
+
+    /**
+     * determines number of rows and columns for board based on number of pairs in the game.
+     * sets this.rows and this.cols
+     *
+     * @param numberOfPairs number of pairs of cards player wants to play with
+     *
+     */
+    private void determineSquare(int numberOfPairs){
+        numberOfPairs *= 2;
+        int cardsPerRow = 5;
+        int cardsPerRowC = 5;
+        int rowTracker = 5;
+        int rows = 2;
+        int cols = 2;
+        int colTracker = 7;
+        while (cardsPerRow < (numberOfPairs)) {
+            //if cards that i am comparing to is less than number of cards in the game, add one row
+            rows += 1;
+            //number of rows changes for every rowTracker number of cards on the board
+            cardsPerRow += rowTracker;
+            //rowTracker increases by two after every increase in rows
+            rowTracker += 2;
+        }
+        while (cardsPerRowC < numberOfPairs){
+            cols += 1;
+            cardsPerRowC += colTracker;
+            colTracker += 2;
+        }
+        this.cols = cols;
+        this.rows = rows;
     }
 
     public static void main(String[] args) {
-            Board b = new Board();
-        }
-}
+        Board b = new Board();
 
+    }
